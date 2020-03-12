7e522585761ed9d03e249e868e5a2f31981cf9ff
rutvij2811 <rutvij.khatri@gmail.com>
2020-03-12 15:46:25 +0530

    Generaly Switch case console ui

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 import lib.Member;
 import test.MemberAdd;
@@ -8,6 +9,7 @@ import test.ViewMembers;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
 		LinkedList<Member> members = null;
 		File file = new File("..\\LibraryProject\\res\\member.ser");
 		if (file.exists()) {
@@ -15,6 +17,29 @@ public class Main {
 		} else {
 			MemberAdd.addMembers();
 			ViewMembers.initMembers(members);
+		}
+		boolean isRunning = true;
+		while(isRunning) {
+			System.out.println("Press 1 to issue the book");
+			System.out.println("Press 2 to issue the multiple books");
+			System.out.println("Press 3 to add member");
+			System.out.println("Press 4 to checkDues");
+			System.out.println("Press 5 to exit");
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1: issueBook();
+				break;
+			case 2: issueBooks();
+			break;
+			case 3: addMember();
+			break;
+			case 4: checkDues();
+			break;
+			case 5: isRunning = false;
+			break;
+			default:System.out.println("Choice not in the list");
+				break;
+			}
 		}
 	}
 }
